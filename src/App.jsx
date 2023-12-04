668c3717d3e91ebae6c03a0cd8f11d17ca1bf186
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-12-04 20:26:35 +0300

    Initial commit with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="header">
+      <div className="container">
+        <h1>Get an Estimate for Workspace</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
