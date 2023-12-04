6e0afb864e0e1053f14b88e2ad373b1c9d9e3c1e
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-12-04 20:51:46 +0300

    Add side header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ function App() {
   return (
     <div className="header">
       <div className="container">
-        <h1>Get an Estimate for Workspace</h1>
+        <div className="wrapper">
+          <h1>Get an Estimate for Workspace</h1>
+        </div>
+
+        <div className="hero-wrapper">
+          <h1>Post Your Requirements</h1>
+          <p className="hero-text">
+            Please fill the form below to receive a quote for workspace. Please
+            add all the details required.
+          </p>
+        </div>
       </div>
     </div>
   );
